76278b4ba1907486b0a6a3d697d1185fadccbee3
mmichaud93 <mmichaud93@gmail.com>
2014-04-09 09:35:39 -0400

    whatever2

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
+	var port = Number(process.env.PORT || 12349);
+
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		var postData = url.parse(request.url, true).query;
@@ -12,7 +14,7 @@ function start(route, handle) {
 		route(handle, pathname, response, request, postData);
 	}
 
-	http.createServer(onRequest).listen(12349);
+	http.createServer(onRequest).listen(port);
 	console.log("Server has started.");
 }
 
